6fcc2f91fcfe9e7fc7ca7450e0a4e792c41e6b10
Abdulaziz6921 <abdulaziztojibayev6@gmail.com>
2022-10-13 11:39:10 +0500

    jimdo logo wrapped in an anchor tag

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ let opens = false;
 let right = document.querySelector("#right_slider");
 const Open = () => {
   if (opens === false) {
-    right.className = `duration-[0.3s] z-20 translate-x-0 absolute left-0 top-0 w-full h-[100vh] bg-white`;
+    right.className = `flex flex-col justify-between duration-[0.3s] z-20 translate-x-0 absolute left-0 top-0 w-full h-[100vh] bg-white`;
     right.parentElement.parentElement.parentElement.className = `overflow-hidden`;
   } else {
     right.className = `translate-x-[700px] `;
